8762ba59ed3b3779c1992c0095119a57988b724f
Sreekesh Iyer <67017933+sreekeshiyer@users.noreply.github.com>
2024-01-28 18:05:03 +0530

    Delete access_dictionary.py

--- access_dictionary.py
+++
@@ -1,9 +0,0 @@
-# Issue of trying to access a key in a dictionary that doesn't exist.
-def access_dictionary_key(key):
-    my_dict = {'name': 'John', 'age': 30}
-    try:
-        return my_dict[key]
-    except KeyError:
-        return f"Error: Key '{key}' not found in the dictionary!"
-
-print(access_dictionary_key('address'))
