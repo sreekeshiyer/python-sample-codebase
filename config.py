9f8d6e9779e8e5f1f9fc4e6215a759e1dfe365af
Sreekesh Iyer <67017933+sreekeshiyer@users.noreply.github.com>
2024-01-28 18:02:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+MONGO_URI = "your_mongo_db_uri"
+AWS_ACCESS_KEY = "your_aws_access_key"
+AWS_SECRET_KEY = "your_aws_secret_key"
+AWS_S3_BUCKET = "your_s3_bucket_name"
+SMTP_SERVER = "your_smtp_server"
+SMTP_PORT = 587
+SMTP_USERNAME = "your_email_username"
+SMTP_PASSWORD = "your_email_password"
+SENDER_EMAIL = "your_sender_email"
+RECIPIENT_EMAIL = "your_recipient_email"
